742e3597b6a1fa215744079b78c929304a55a68b
dangquang17 <tdq171@gmail.com>
2024-01-02 16:48:50 +0700

    doneee

--- controllers/view.controller.js
+++ controllers/view.controller.js
@@ -11,21 +11,15 @@ const homeView = (req, res) => {
     res.render('home', { active: 'home', role: req.session.account.role });
 }
 
-const handleView = (req, res) => {
-    const source = req.query.source;
-    if (source === 'edit') {
-        const assetID = req.query.id;
-        res.render('edit-asset');
-    }
-    res.render('handle-asset');
-}
-
 const inventoryView = (req, res) => {
-    res.render('inventory', { isFarmer: req.session.account.role === 'farmer', script: 'inventory', active: 'inventory', username: JSON.stringify(req.session.account)});
+    res.render('inventory', { isFarmer: req.session.account.role === 'farmer', 
+    script: 'inventory', active: 'inventory', 
+    username: JSON.stringify(req.session.account), 
+    role: req.session.account.role});
 }
 
 const purchaseView = (req, res) => {
-    res.render('purchase-asset', { script: 'purchase-asset', active: 'purchase' });
+    res.render('purchase-asset', { script: 'purchase-asset', active: 'purchase', role: req.session.account.role });
 }
 
-module.exports = { loginView, registerView, homeView, handleView, inventoryView, purchaseView };
+module.exports = { loginView, registerView, homeView, inventoryView, purchaseView };
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ router.get('/register', controller.registerView);
 router.get('/', auth.isLogged, controller.homeView);
 router.get('/purchase', auth.isCompany, controller.purchaseView);
 router.get('/inventory', auth.isLogged, controller.inventoryView);
-router.get('/handle', auth.isFarmer, controller.handleView);
 
 router.use('/account', accountRouter);
 router.use('/transfer', auth.isLogged, transferRouter);
